c111472e9d07c5fb2130f8e9ba163c86d7983c43
Lucas Gris <gris@alunos.utfpr.edu.br>
2022-10-17 06:01:21 -0300

    rasa

--- interacao_nlu/src/rasa_interpreter.py
+++ interacao_nlu/src/rasa_interpreter.py
@@ -3,12 +3,12 @@ import os
 
 import rospy
 from std_msgs.msg import String
-#from roboga_nlu.srv import Nlu, NluResponse
+from interacao_nlu.srv import Nlu, NluResponse
 from rasa_nlu.model import Interpreter
 
 if __name__ == "__main__":
     directory = os.path.dirname(os.path.realpath(__file__))
-    rospy.init_node('rasa_nlu', anonymous=True)
+    rospy.init_node('interacao_nlu', anonymous=True)
     interpreter = Interpreter.load(directory+"/rasa/models/nlu")
     
     def handler(req):
